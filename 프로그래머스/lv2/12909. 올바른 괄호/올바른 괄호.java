f782b1c80eabe9e23f54c92632b4d136866990c0
Park Jaehyeong <90443006+hyeong0917@users.noreply.github.com>
2023-08-12 23:55:58 +0900

    [level 2] Title: 올바른 괄호, Time: 18.88 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                stack.add(c);
+            }
+            else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        if(!stack.isEmpty()) return false;
+
+        return true;
+    }
+}
