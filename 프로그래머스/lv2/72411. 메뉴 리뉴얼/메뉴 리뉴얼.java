1369dd87224b8fb6eafde3021cda99119273951a
Park Jaehyeong <90443006+hyeong0917@users.noreply.github.com>
2023-10-02 05:46:24 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 21.51 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    List<String> answerList = new ArrayList<>();
+    Map<String, Integer> hashMap = new HashMap<>();
+    
+    public String[] solution(String[] orders, int[] course) {
+        for(int i = 0; i < orders.length; i++) {
+            char[] arr = orders[i].toCharArray();
+            Arrays.sort(arr);
+            orders[i] = String.valueOf(arr);
+        }
+        
+        for(int courseLength : course) {
+            for(String order : orders) {
+                combination("", order, courseLength);
+            }
+            if(!hashMap.isEmpty()) {
+                List<Integer> countList = new ArrayList<>(hashMap.values());
+                int max = Collections.max(countList);
+                    
+                if(max > 1) {
+                    for(String key : hashMap.keySet()) {
+                        if(hashMap.get(key) == max) answerList.add(key);
+                    }
+                }                    
+                hashMap.clear();
+            }
+        }
+        
+        Collections.sort(answerList);
+        
+        String[] answer = new String[answerList.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+        return answer;
+    }
+    public void combination(String order, String others, int count) {
+        //탈출조건
+        if(count == 0) {
+            hashMap.put(order, hashMap.getOrDefault(order, 0) + 1);
+            return;
+        }
+        
+        //수행조건
+        for(int i = 0; i < others.length(); i++) {
+            combination(order + others.charAt(i), others.substring(i + 1), count - 1);
+        }
+    }
+}
